37ef417ddf3e0141a8bbe18594df89d31fe114e0
OzbejOfficial <ozbejpavc@gmail.com>
2025-11-30 04:18:45 +0100

    Multitenancy fix

--- audit-service/src/main/kotlin/traversium/audit/AuditApplication.kt
+++ audit-service/src/main/kotlin/traversium/audit/AuditApplication.kt
@@ -1,9 +1,16 @@
 package traversium.audit
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
+import traversium.audit.kafka.KafkaProperties
+import traversium.commonmultitenancy.FlywayTenantMigration
+import traversium.commonmultitenancy.MultiTenantAutoConfiguration
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaProperties::class)
+@Import(MultiTenantAutoConfiguration::class, FlywayTenantMigration::class)
 class AuditApplication
 
 fun main(args: Array<String>) {
--- audit-service/src/test/kotlin/traversium/audit/AuditApplicationTests.kt
+++ audit-service/src/test/kotlin/traversium/audit/AuditApplicationTests.kt
@@ -1,9 +1,21 @@
 package traversium.audit
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import traversium.audit.security.TestMultitenancyConfig
 
-@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@ActiveProfiles("test")
+@SpringBootTest(
+    classes = [
+        AuditApplication::class,
+        TestMultitenancyConfig::class
+    ]
+)
+@DirtiesContext
 class AuditApplicationTests {
 
     @Test
